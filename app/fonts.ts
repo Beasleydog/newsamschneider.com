caa3469aa121be2ae5333d96fc123f38cab5d3bf
Beasleydog <beasleypero@gmail.com>
2025-01-31 17:41:23 -0500

    attempt to fix fonts

--- app/fonts.ts
+++ app/fonts.ts
@@ -6,21 +6,25 @@ import localFont from "next/font/local";
 
 const jersey15 = Jersey_15({
   subsets: ["latin"],
-  weight: ["400"],
+  display: "swap",
+  weight: "400",
 });
 
 const ptSerif = PT_Serif({
   subsets: ["latin"],
+  display: "swap",
   weight: ["400", "700"],
 });
 
 const rammettoOne = Rammetto_One({
   subsets: ["latin"],
+  display: "swap",
   weight: ["400"],
 });
 
 const luckiestGuy = Luckiest_Guy({
   subsets: ["latin"],
+  display: "swap",
   weight: ["400"],
 });
 
